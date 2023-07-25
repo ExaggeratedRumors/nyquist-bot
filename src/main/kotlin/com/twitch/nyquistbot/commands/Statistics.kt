01c6bdf85b1c7be822cb48608fb68cefaee9bccc
exaggeratedrumors <exaggeratedrumors@gmail.com>
2023-07-25 13:51:01 +0200

    Statistics command

--- src/main/kotlin/com/twitch/nyquistbot/commands/Statistics.kt
+++ src/main/kotlin/com/twitch/nyquistbot/commands/Statistics.kt
@@ -5,5 +5,9 @@ import com.twitch.nyquistbot.transmission.Sender
 
 class Statistics: Command() {
     override fun execute(chatMessage: Message, sender: Sender) {
+        val newMessage = chatMessage.clone()
+        newMessage.author = "bot"
+        newMessage.chatText = chatMessage.author
+        sender.sendChatMessage(newMessage)
     }
 }
